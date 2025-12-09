f2a4d0255aabc9ea63a2e8e34598cbaaa8769bf6
mdlq <mhqmain@gmail.com>
2025-12-09 14:38:52 +0700

    Disable diagnostics window when there is only warnings and no errors (vimtex)

--- lua/globals.lua
+++ lua/globals.lua
@@ -9,3 +9,6 @@ vim.g.vimtex_compiler_latexmk = {
 	["aux_dir"] = latex_cwd .. "/.out/aux",
 	["out_dir"] = latex_cwd .. "/.out/build",
 }
+
+-- Disable diagnostics window when there is only warnings and no errors
+vim.g.vimtex_quickfix_open_on_warning = 0
