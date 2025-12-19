57a340e2fa9ca7dbc88c77f72f17d431f6968b27
mdlq <mhqmain@gmail.com>
2025-12-19 07:11:10 +0700

    Create two separate function/keymap for go to definition, from current buffer and in new tab

--- lua/user/language-server-settings/handler.lua
+++ lua/user/language-server-settings/handler.lua
@@ -31,7 +31,8 @@ M.on_attach = function(client, bufnr)
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
 	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", definition_new_tab, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gT", definition_new_tab, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
 	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
